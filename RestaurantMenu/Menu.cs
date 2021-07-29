d49c081b97b2ef06fbe6565a8f87f63e9814e57c
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-07-28 20:05:01 -0500

    finished studio

--- RestaurantMenu/Menu.cs
+++ RestaurantMenu/Menu.cs
@@ -3,8 +3,22 @@ namespace RestaurantMenu
 {
     public class Menu
     {
-        public Menu()
+        public string MenuItem { get; set; }
+        public DateTime LastTimeUpdated { get; set; }
+
+        //or
+        //[System.Serializable]
+        //public struct DateTime : IComparable, IComparable<DateTime>, IConvertible, IEquatable<DateTime>, IFormattable, System.Runtime.Serialization.ISerializable
+
+        ////or
+        //public struct DateTime { get; set; }
+
+
+
+        public Menu(string menuItem, DateTime lastTimeUpdated)
         {
+            MenuItem = menuItem;
+            LastTimeUpdated = lastTimeUpdated;
         }
     }
 }
--- RestaurantMenu/MenuItem.cs
+++ RestaurantMenu/MenuItem.cs
@@ -3,8 +3,25 @@ namespace RestaurantMenu
 {
     public class MenuItem
     {
-        public MenuItem()
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public bool IsNew { get; set; }
+
+
+        public MenuItem(string name, string description, string category, double price, bool isNew)
         {
+            Name = name;
+            Description = description;
+            Category = category;
+            Price = price;
+            IsNew = isNew;
         }
+
+
+
     }
+
+
 }
